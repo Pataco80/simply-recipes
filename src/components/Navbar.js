18492cec7348dcc800f26424987b38cc626caa4a
Ricardo Do Vale <pataco80@gmail.com>
2021-04-18 20:31:53 +0200

    Create Navbar
    create Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// Import Components Gatsby
 import { Link } from 'gatsby'
+
+// Import Components for App
+import logo from '../images/logo.svg'
+import { TextAlignJustify } from '@styled-icons/fluentui-system-filled/TextAlignJustify'
+
+// Import styled-components
+import styled from 'styled-components'
+
+// markup
 const Navbar = () => {
+  // variables
+  const [show, setShow] = useState(false)
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about/'>About</Link>
-        </li>
-        <li>
-          <Link to='/recipies/'>Recepies</Link>
-        </li>
-        <li>
-          <Link to='/tags/'>Tags</Link>
-        </li>
-        <li>
-          <Link to='/contact/'>Contact</Link>
-        </li>
-      </ul>
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <Link to='/'>
+            <img src={logo} alt='logo' />
+          </Link>
+          <button className='nav-btn' onClick={() => setShow(!show)}>
+            <ToggleIcon />
+          </button>
+        </div>
+        <div className={show ? `nav-links show-links` : `nav-links`}>
+          <Link
+            to='/'
+            className='nav-link'
+            activeClassName='active-link'
+            onClick={() => setShow(false)}
+          >
+            Home
+          </Link>
+          <Link
+            to='/recipes/'
+            className='nav-link'
+            activeClassName='active-link'
+            onClick={() => setShow(false)}
+          >
+            Recipes
+          </Link>
+          <Link
+            to='/tags/'
+            className='nav-link'
+            activeClassName='active-link'
+            onClick={() => setShow(false)}
+          >
+            Tags
+          </Link>
+          <Link
+            to='/about/'
+            className='nav-link'
+            activeClassName='active-link'
+            onClick={() => setShow(false)}
+          >
+            About
+          </Link>
+          <div className='nav-link contact-link'>
+            <Link to='/contact/' className='btn'>
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
+
+const ToggleIcon = styled(TextAlignJustify)`
+  height: auto;
+  width: 1.5rem;
+`
 
 export default Navbar
