2547d2eac7ca2bff849bc338caa42067bd91a778
Ricardo Do Vale <pataco80@gmail.com>
2021-04-19 19:55:39 +0200

    create hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,35 @@ import React from 'react'
 
 // Import React Hooks
 
+// Import Components Gatsby
+import { StaticImage } from 'gatsby-plugin-image'
+
 // Import Components for App
 import Layout from '../components/Layout'
 
 // Import styled-components, styledElements and helpers
 
 // markup
-const HomePage = () => {
+const HomePage = ({ children }) => {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <main className='page'>
+        <header className='hero'>
+          <StaticImage
+            src='../images/main.jpeg'
+            alt='hero image'
+            className='hero-img'
+            placeholder='blurred'
+            layout='fullWidth'
+          />
+          <div className='hero-container'>
+            <div className='hero-text'>
+              <h1>Simply Recipes</h1>
+              <h4>No Fluff, Just Recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   )
 }
