e7a501646d52f354897cf8d4b1268011a42deed7
Ricardo Do Vale <pataco80@gmail.com>
2021-04-21 15:29:16 +0200

    Contentful connexion to variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: 'Simply Recipes',
@@ -38,7 +42,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `nwh121i3omrp`,
+        spaceId: process.env.CONTENTFUL_API_SPACEID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
